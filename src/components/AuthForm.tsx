38aabbebea07382c6adb4835ed513f8764561057
DonkeykongM <mathias_bahko@hotmail.com>
2025-09-26 16:32:47 +0000

    Fix authentication and password visibility issues

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -206,13 +206,15 @@ const AuthForm: React.FC<AuthFormProps> = ({ onSignIn, onBack }) => {
                     required
                     disabled={loading}
                   />
-                  <button
-                    type="button"
-                    onClick={() => setShowPassword(!showPassword)}
-                    className="absolute right-3 top-1/2 transform -translate-y-1/2 text-neutral-400 hover:text-neutral-600"
-                  >
-                    {showPassword ? <EyeOff className="w-5 h-5" /> : <Eye className="w-5 h-5" />}
-                  </button>
+                  {isLogin && (
+                    <button
+                      type="button"
+                      onClick={() => setShowPassword(!showPassword)}
+                      className="absolute right-3 top-1/2 transform -translate-y-1/2 text-neutral-400 hover:text-neutral-600"
+                    >
+                      {showPassword ? <EyeOff className="w-5 h-5" /> : <Eye className="w-5 h-5" />}
+                    </button>
+                  )}
                 </div>
               </div>
 
